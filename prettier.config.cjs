fe374f49e7c399d9ec3342cebfcf72a9fa68d78c
sorrow scavenger <hatim.touil0@gmail.com>
2025-05-02 21:41:24 +0100

    initialize git

---
+++ prettier.config.cjs
@@ -0,0 +1,12 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  arrowParens: 'always',
+  plugins: ['prettier-plugin-tailwindcss'],
+};
