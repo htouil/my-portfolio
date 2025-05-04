58741cbae6ce9e3fd34740c4bb4f8e485c20dba8
sorrow scavenger <hatim.touil0@gmail.com>
2025-05-04 15:57:15 +0100

    added profil picture

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,49 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+// import { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div className="flex justify-around">
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="flex">
+        <img
+          src="/hatim_picture.jpg"
+          alt="profil_picture"
+          className="w-96"
+        ></img>
+        <div className="self-center grow text-white">
+          <h1 className="text-center text-3xl ">My Portfolio</h1>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
 
 export default App;
+// import reactLogo from './assets/react.svg';
+// import viteLogo from '/vite.svg';
+// const [count, setCount] = useState(0);
+
+// return (
+//   <>
+//     <div className="flex justify-around">
+//       <a href="https://vite.dev" target="_blank">
+//         <img src={viteLogo} className="logo" alt="Vite logo" />
+//       </a>
+//       <a href="https://react.dev" target="_blank">
+//         <img src={reactLogo} className="logo react" alt="React logo" />
+//       </a>
+//     </div>
+//     <h1>Vite + React</h1>
+//     <div className="card">
+//       <button onClick={() => setCount((count) => count + 1)}>
+//         count is {count}
+//       </button>
+//       <p>
+//         Edit <code>src/App.tsx</code> and save to test HMR
+//       </p>
+//     </div>
+//     <p className="read-the-docs">
+//       Click on the Vite and React logos to learn more
+//     </p>
+//   </>
+// );
