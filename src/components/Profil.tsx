7cc0fe568b581bcc56f560fae4fa7644b36ae4e7
htouil <hatim.touil0@gmail.com>
2025-05-08 03:19:09 +0100

    starting with responsivity

--- src/components/Profil.tsx
+++ src/components/Profil.tsx
@@ -6,14 +6,14 @@ const Profil = () => {
       <img
         src="/hatim_picture.jpg"
         alt="profil_picture"
-        className="w-96 rounded-xl p-2"
+        className="w-64 rounded-xl p-2"
       ></img>
-      <div className="flex flex-col place-content-center-safe grow text-white gap-7 mt-10">
-        <p className="text-center self-start text-3xl ml-20">
+      <div className="flex flex-col place-content-center-safe grow text-white gap-4 mt-0">
+        <p className="text-center self-start text-xl ml-10">
           Hello dear visitor,
           <br />I am Hatim TOUIL
         </p>
-        <div className="flex self-center border-4 border-black rounded-3xl w-[450px] h-80 ml-20">
+        <div className="flex self-center border-4 border-black rounded-3xl w-80 h-64 ml-10">
 
 		</div>
       </div>
