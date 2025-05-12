1aa847bf12507e03e07398d9fba59cf5ae604aff
htouil <hatim.touil0@gmail.com>
2025-05-12 05:37:27 +0100

    Projects component structure sat up

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,12 +1,84 @@
 const Projects = () => {
   return (
-    <div className="m-4 grid grid-cols-3 grid-rows-2 gap-4 rounded-xl bg-slate-950/60 p-5">
-      <div className="bg-gray-200 p-4">Item 1</div>
-      <div className="bg-gray-200 p-4">Item 2</div>
-      <div className="bg-gray-200 p-4">Item 3</div>
-      <div className="bg-gray-200 p-4">Item 4</div>
-      <div className="bg-gray-200 p-4">Item 5</div>
-      <div className="bg-gray-200 p-4">Item 6</div>
+    <div className="m-4 grid grid-cols-3 grid-rows-2 place-items-center gap-x-4 gap-y-6 rounded-xl bg-slate-950/60 p-5">
+      <div className="flex flex-col rounded-lg border border-gray-400">
+        <h2 className="rounded-t-lg border border-gray-400 bg-blue-400 p-1 text-center text-white">
+          so_long
+        </h2>
+        <div className="border border-t-gray-400">
+          <img
+            src="/so_long_cover.png"
+            alt="so_long_cover"
+            className="size-44 rounded-b-lg"
+          />
+          <p className="hidden">so this is it</p>
+        </div>
+      </div>
+      <div className="flex flex-col rounded-lg border border-gray-400">
+        <h2 className="rounded-t-lg border border-gray-400 bg-blue-400 p-1 text-center text-white">
+          so_long
+        </h2>
+        <div className="border border-t-gray-400">
+          <img
+            src="/push_swap_cover.jpg"
+            alt="push_swap_cover"
+            className="size-44 rounded-b-lg"
+          />
+          <p className="hidden">so this is it</p>
+        </div>
+      </div>
+      <div className="flex flex-col rounded-lg border border-gray-400">
+        <h2 className="rounded-t-lg border border-gray-400 bg-blue-400 p-1 text-center text-white">
+          so_long
+        </h2>
+        <div className="border border-t-gray-400">
+          <img
+            src="/minishell_cover.jpg"
+            alt="minishell_cover"
+            className="size-44 rounded-b-lg"
+          />
+          <p className="hidden">so this is it</p>
+        </div>
+      </div>
+      <div className="flex flex-col rounded-lg border border-gray-400">
+        <h2 className="rounded-t-lg border border-gray-400 bg-blue-400 p-1 text-center text-white">
+          so_long
+        </h2>
+        <div className="border border-t-gray-400">
+          <img
+            src="/inception_cover.jpg"
+            alt="inception_cover"
+            className="size-44 rounded-b-lg"
+          />
+          <p className="hidden">so this is it</p>
+        </div>
+      </div>
+      <div className="flex flex-col rounded-lg border border-gray-400">
+        <h2 className="rounded-t-lg border border-gray-400 bg-blue-400 p-1 text-center text-white">
+          so_long
+        </h2>
+        <div className="border border-t-gray-400">
+          <img
+            src="/ft_irc_cover.png"
+            alt="ft_irc_cover"
+            className="size-44 rounded-b-lg"
+          />
+          <p className="hidden">so this is it</p>
+        </div>
+      </div>
+      <div className="flex flex-col rounded-lg border border-gray-400">
+        <h2 className="rounded-t-lg border border-gray-400 bg-blue-400 p-1 text-center text-white">
+          so_long
+        </h2>
+        <div className="border border-t-gray-400">
+          <img
+            src="/ft_transcendence_cover.png"
+            alt="ft_transcendence_cover"
+            className="size-44 rounded-b-lg"
+          />
+          <p className="hidden">so this is it</p>
+        </div>
+      </div>
     </div>
   );
 };
